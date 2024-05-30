df0e59584a65f2333b23bc7892e20b06d7bddcf2
Arjan <asiemons@live.com>
2024-05-30 11:21:15 +0200

    debug

--- components/zehnder/zehnder.cpp
+++ components/zehnder/zehnder.cpp
@@ -509,8 +509,8 @@ void ZehnderRF::setSpeed(const uint8_t paramSpeed, const uint8_t paramTimer) {
     (void) memset(this->_txFrame, 0, FAN_FRAMESIZE);  // Clear frame data
 
     // Build frame
-    pFrame->rx_type = this->config_.fan_main_unit_type;
-    pFrame->rx_id = this->config_.fan_main_unit_id;
+    pFrame->rx_type = FAN_TYPE_MAIN_UNIT;
+    pFrame->rx_id = FAN_TYPE_BROADCAST;
     pFrame->tx_type = this->config_.fan_my_device_type;
     pFrame->tx_id = this->config_.fan_my_device_id;
     pFrame->ttl = FAN_TTL;
