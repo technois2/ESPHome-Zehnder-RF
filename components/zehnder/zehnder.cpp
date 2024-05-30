5e8d69a6f61e56eb518189ed91739a43640fa938
Arjan <asiemons@live.com>
2024-05-30 11:44:59 +0200

    debug

--- components/zehnder/zehnder.cpp
+++ components/zehnder/zehnder.cpp
@@ -478,7 +478,7 @@ void ZehnderRF::queryDevice(void) {
   // Build frame
   //pFrame->rx_type = this->config_.fan_main_unit_type;
   pFrame->rx_type = FAN_TYPE_MAIN_UNIT;
-  pFrame->rx_id = this->config_.fan_main_unit_id;
+  pFrame->rx_id = FAN_TYPE_BROADCAST;
   pFrame->tx_type = 0x03;
   pFrame->tx_id = this->config_.fan_my_device_id;
   pFrame->ttl = FAN_TTL;
